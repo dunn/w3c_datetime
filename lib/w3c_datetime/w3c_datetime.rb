0499a3330eb88cf80b3400c18a306dfc76213348
Alex Dunn <dunn.alex@gmail.com>
2017-06-28 11:40:00 -0700

    W3cDatetime.parse: replace Time with DateTime
    This allows us to catch invalid dates like 1940-02-30.

--- lib/w3c_datetime/w3c_datetime.rb
+++ lib/w3c_datetime/w3c_datetime.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 # Handle W3 Date and Time formats
 #
 # Handle W3 Date and Time formats as specified in http://www.w3.org/TR/NOTE-datetime
@@ -11,11 +13,11 @@
 #
 # @author Michal Pawlowski <misza222@gmail.com>
 class W3cDatetime
-
   # Parse date string in http://www.w3.org/TR/NOTE-datetime format
   #
-  # @param [String] date_str date represented as string in http://www.w3.org/TR/NOTE-datetime format
-  # @return [Time] Time object
+  # @param [String] date_str date represented as string in
+  #     http://www.w3.org/TR/NOTE-datetime format
+  # @return [DateTime]
   def self.parse(date_str)
     date_regexp = /^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})(?:T(?<hour>\d{2}):(?<minute>\d{2}):?(?<second>\d{2})?\.?(?<milisecond>\d{1,2})?(?:(?<timezone>.+)|Z)?)?$/
     parsed = date_regexp.match(date_str)
@@ -25,7 +27,7 @@ class W3cDatetime
             "#{date_str} is not W3C-valid (http://www.w3.org/TR/NOTE-datetime)"
     end
 
-    Time.new(
+    DateTime.new(
       parsed[:year].to_i,
       parsed[:month].to_i,
       parsed[:day].to_i,
@@ -36,15 +38,11 @@ class W3cDatetime
     )
   end
 
-  class << self
-    private
-    def get_timezone(timezone)
-      if timezone == 'Z'
-        timezone = '+00:00'
-      elsif timezone == ''
-        timezone = nil
-      end
-
+  def self.get_timezone(timezone)
+    case timezone
+    when 'Z', '', nil
+      '+00:00'
+    else
       timezone
     end
   end
