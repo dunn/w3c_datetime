3ae13256e7affd46028396ee6df04eb3d31aa5cb
Alex Dunn <dunn.alex@gmail.com>
2017-06-28 11:11:00 -0700

    W3cDatetime.parse: don't quash errors
    The errors can be useful to downstream applications.

--- lib/w3c_datetime/w3c_datetime.rb
+++ lib/w3c_datetime/w3c_datetime.rb
@@ -19,10 +19,21 @@ class W3cDatetime
   def self.parse(date_str)
     date_regexp = /^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})(?:T(?<hour>\d{2}):(?<minute>\d{2}):?(?<second>\d{2})?\.?(?<milisecond>\d{1,2})?(?:(?<timezone>.+)|Z)?)?$/
     parsed = date_regexp.match(date_str)
-    begin
-      Time.new(parsed[:year].to_i, parsed[:month].to_i, parsed[:day].to_i, parsed[:hour].to_i, parsed[:minute].to_i, parsed[:second].to_i, get_timezone(parsed[:timezone])) unless parsed.nil?
-    rescue ArgumentError, TypeError
+
+    if parsed.nil?
+      raise ArgumentError,
+            "#{date_str} is not W3C-valid (http://www.w3.org/TR/NOTE-datetime)"
     end
+
+    Time.new(
+      parsed[:year].to_i,
+      parsed[:month].to_i,
+      parsed[:day].to_i,
+      parsed[:hour].to_i,
+      parsed[:minute].to_i,
+      parsed[:second].to_i,
+      get_timezone(parsed[:timezone])
+    )
   end
 
   class << self
