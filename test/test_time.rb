e13396c98c89a66dab49ba2a3ffecc23a0fe62b8
Alex Dunn <dunn.alex@gmail.com>
2017-06-28 11:46:13 -0700

    gemspec: add minitest to development dependencies

--- test/test_time.rb
+++ test/test_time.rb
@@ -1,4 +1,3 @@
-require 'minitest/spec'
 require 'minitest/autorun'
 require 'w3c_datetime'
 
@@ -6,8 +5,8 @@ describe Time do
   describe '#to_w3c_datetime' do
   	it 'should return string representing time in W3C date time format' do
   	  t = Time.new
-  	  
+
   	  t.must_be_within_delta W3cDatetime::parse(t.to_w3c_datetime), 1
   	end
-  end	
+  end
 end
--- w3c_datetime.gemspec
+++ w3c_datetime.gemspec
@@ -14,5 +14,6 @@ Gem::Specification.new do |spec|
   spec.required_ruby_version = '>=1.9.2'
 
   spec.add_development_dependency "bundler", "~> 1.3"
+  spec.add_development_dependency "minitest"
   spec.add_development_dependency "rake"
 end
